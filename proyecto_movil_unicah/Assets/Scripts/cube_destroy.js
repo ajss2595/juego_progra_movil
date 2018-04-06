6ffdf250b2bfbf949fded7abe2343c913becaa8d
ajss2595 <34406819+ajss2595@users.noreply.github.com>
2018-04-05 21:51:54 -0600

    avance del gameplay
    se agregaron las monedas y ciertas aspectos esteticos se modificaron

---
+++ proyecto_movil_unicah/Assets/Scripts/cube_destroy.js
@@ -0,0 +1,38 @@
+﻿
+var XPos : float;
+var YPos : float;
+var ZPos : float;
+var Waiting : float = 0.02;
+
+function Start () {
+    XPos = transform.position.x;
+    YPos = transform.position.y;
+    ZPos = transform.position.z;
+}
+
+function OnTriggerEnter (col : Collider) {
+
+    if (col.gameObject.tag == "Player") {
+        this.transform.position = Vector3(XPos, YPos+0.1, ZPos);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos+0.2, ZPos);
+        yield WaitForSeconds(Waiting);
+        transform.GetComponent.<Collider>().isTrigger = false;
+        this.transform.position = Vector3(XPos, YPos+0.3, ZPos+0.5);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos+0.4, ZPos+1.0);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos-0.1, ZPos+1.5);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos-0.6, ZPos+2.0);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos-1.6, ZPos+2.0);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos-2.6, ZPos+2.0);
+        yield WaitForSeconds(Waiting);
+        this.transform.position = Vector3(XPos, YPos-4.0, ZPos+2.0);
+        yield WaitForSeconds(0.25);
+        transform.GetComponent.<Collider>().isTrigger = true;
+        Destroy(gameObject);
+    }
+}
---
+++ proyecto_movil_unicah/Assets/Scripts/cube_nodestroy.js
@@ -0,0 +1,22 @@
+﻿
+var XPos : float;
+var YPos : float;
+var ZPos : float;
+
+function Start ()
+{
+    XPos = transform.position.x;
+    YPos = transform.position.y;
+    ZPos = transform.position.z;
+}
+
+function OnTriggerEnter (col : Collider) {
+    transform.GetComponent.<Collider>().isTrigger = false;
+    if (col.gameObject.tag == "Player") {
+        this.transform.position = Vector3(XPos, YPos+0.2, ZPos);
+        yield WaitForSeconds(0.08);
+        this.transform.position = Vector3(XPos, YPos, ZPos);
+        yield WaitForSeconds(0.25);
+        transform.GetComponent.<Collider>().isTrigger = true;
+    }
+}
